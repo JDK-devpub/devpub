05385b211785587b8250a39f75672c38ac23e795
buyback <davidtanugrah@gmail.com>
2016-10-12 23:34:30 +0800

    Version 1.8
    aa

--- index.php
+++ index.php
@@ -215,7 +215,7 @@ span.psw {
                         <input type="text" placeholder="Enter Username" name="uname" required>
 
                         <label><b>Password</b></label>
-                        <input type="password" placeholder="Enter Password" name="psw" required>
+                        <input type="password" placeholder="Enter Password"  name="psw" required>
                           
                         <button type="submit">Login</button>
                         <input type="checkbox" checked="checked"> Remember me
